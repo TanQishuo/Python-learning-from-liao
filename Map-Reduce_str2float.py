96fa5f4d19397f1478c30d845e491613e6cbbdab
TanQishuo <37700238+TanQishuo@users.noreply.github.com>
2018-05-09 22:22:39 +0800

    Add files via upload

---
+++ Map-Reduce_str2float.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  9 22:07:03 2018
+
+@author: 123456
+"""
+
+#利用map和reduce编写一个str2float函数，把字符串'123.456'转换成浮点数123.456：
+def str2float(s):
+    pass
+#print('str2float(\'123.456\') =', str2float('123.456'))
+#if abs(str2float('123.456') - 123.456) < 0.00001:
+#    print('测试成功!')
+#else:
+#    print('测试失败!')
