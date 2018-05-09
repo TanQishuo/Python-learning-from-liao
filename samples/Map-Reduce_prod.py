6d5223b9158572e6101f75536ec25ae594db029c
TanQishuo <37700238+TanQishuo@users.noreply.github.com>
2018-05-09 22:22:05 +0800

    Add files via upload

---
+++ samples/Map-Reduce_prod.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  9 22:06:50 2018
+
+@author: 123456
+"""
+
+
+#Python提供的sum()函数可以接受一个list并求和，请编写一个prod()函数，可以接受一个list并利用reduce()求积：
+def prod(L):
+    pass
+# 测试：
+#print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
+#if prod([3, 5, 7, 9]) == 945:
+#    print('测试成功!')
+#else:
+#    print('测试失败!')
